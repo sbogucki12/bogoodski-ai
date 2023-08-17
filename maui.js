e21c3d7e1e2eff26f93194d4f970d27f314a1690
sbogucki12 <sbogucki@mail.usf.edu>
2023-08-16 19:33:46 -0700

    finishing up Maui story

--- maui.js
+++ maui.js
@@ -5,9 +5,15 @@ document.getElementById('revealButton').addEventListener('click', function() {
         // Reveal the first hidden section
         hiddenSections[0].classList.remove('hidden');
         
-        // If that was the last section, hide or disable the button
+        // If that was the last section, hide the reveal button and show the home button
         if (hiddenSections.length === 1) {
-            this.style.display = 'none'; // or this.disabled = true; if you prefer to disable the button
+            this.style.display = 'none';
+            document.getElementById('homeButton').style.display = 'block';
         }
     }
+});
+
+// Assuming the homepage is at the root. Adjust the href as necessary.
+document.getElementById('homeButton').addEventListener('click', function() {
+    window.location.href = '/';
 });
